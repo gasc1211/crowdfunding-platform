6eeb67203a79d8065ea6fd148a88dcd87ed84dc0
andre <andre.herrera@unah.hn>
2024-10-18 10:03:34 -0500

    Mejoras LP

--- app/page.tsx
+++ app/page.tsx
@@ -9,8 +9,8 @@ export default function HomePage() {
     <>
       <div className="min-h-full">
         < main className="flex min-h-screen flex-col p-6" >
-          <div className="mt-4 flex grow flex-col gap-4 md:flex-row">
-            <div className="flex flex-col justify-center gap-6 rounded-lg bg-gray-50 bg-opacity-30 px-6 py-10 md:w-2/5 md:px-20">
+          <div className="mt-4 flex grow flex-col gap-4 md:flex-row justify-between">
+            <div className="flex flex-col justify-center gap-6 rounded-3xl bg-gray-50 bg-opacity-30 px-6 py-10 md:w-3/5 md:px-20 m-6">
               <div />
               <p
                 className={`${lusitana.className} text-xl text-gray-800 md:text-3xl md:leading-normal`}>
@@ -34,12 +34,12 @@ export default function HomePage() {
                 </Link>
               </div>
             </div>
-            <div className="flex items-center justify-center p-6 md:w-3/5 md:px-28 md:py-12">
+            <div className="flex items-center justify-center p-6 md:w-4/5 md:px-28 md:py-12">
               <Image
                 src="/productor.webp"
                 width={1000}
                 height={760}
-                className="hidden md:block"
+                className="hidden md:block rounded-3xl"
                 alt="Screenshots of the dashboard project showing desktop version"
                 priority= {true}
               />
@@ -47,7 +47,7 @@ export default function HomePage() {
                 src="/productor.webp"
                 width={560}
                 height={620}
-                className="block md:hidden"
+                className="block md:hidden rounded-3xl"
                 alt="Screenshot of the dashboard project showing desktop version"
                 priority= {true}
               />
