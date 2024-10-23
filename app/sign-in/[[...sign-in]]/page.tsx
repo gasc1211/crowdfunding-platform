696a9571bd28742489285f3719bec375339c5255
GASC1211 <48170476+Gasc1211@users.noreply.github.com>
2024-10-22 23:24:19 -0600

    Rename sign in and sign up pages

--- app/sign-in/[[...sign-in]]/page.tsx
+++ app/sign-in/[[...sign-in]]/page.tsx
@@ -1,6 +1,6 @@
 import { SignIn } from '@clerk/nextjs'
 
-export default function Page() {
+export default function SignInPage() {
     return(
         <div className='flex justify-center content-center'>
             <div className='py-12'>
--- app/sign-up/[[...sign-up]]/page.tsx
+++ app/sign-up/[[...sign-up]]/page.tsx
@@ -1,7 +1,7 @@
 import { SignUp } from '@clerk/nextjs'
 
-export default function Page() {
-    return(
+export default function SignUpPage() {
+    return (
         <div className='flex justify-center content-center'>
             <div className='py-12'>
                 <SignUp />
