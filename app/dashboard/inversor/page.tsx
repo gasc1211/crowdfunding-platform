e180723c05581b207ec17b0f0910fe770eae36fd
GASC1211 <48170476+Gasc1211@users.noreply.github.com>
2024-10-24 15:55:40 -0600

    Get projects from database

--- app/dashboard/inversor/page.tsx
+++ app/dashboard/inversor/page.tsx
@@ -1,30 +1,13 @@
 import Hero from "@/app/ui/components/Hero";
 import ProjectCard from "@/app/ui/components/ProjectCard";
+import { createClient } from "@/utils/supabase/client";
 
-export default function InversorDashboard() {
-    const projects = [
-        {
-            title: "Creación de huerto comunitario",
-            description:
-                "Desarrollo de un espacio para cultivo de consumo local.",
-            image: "/proyecto1.png",
-            progress: 70,
-        },
-        {
-            title: "Producción de alimentos orgánicos",
-            description:
-                "Gran impacto en la salud y el bienestar de la población local al mejorar la calidad de los alimentos y la producción agrícola.",
-            image: "/proyecto1.png",
-            progress: 45,
-        },
-        {
-            title: "Mejora de calidad de suelos",
-            description:
-                "Se lleva a cabo mediante la reducción de la contaminación, el uso eficiente de los recursos naturales y el manejo adecuado de los desechos.",
-            image: "/proyecto1.png",
-            progress: 60,
-        },
-    ];
+export default async function InversorDashboard() {
+
+    const supabase = createClient();
+
+    const results = await supabase.from("projects").select("*").limit(5);
+    const projects = results.data;
 
     return (
         <>
@@ -32,7 +15,7 @@ export default function InversorDashboard() {
                 <main className="flex-grow">
                     <Hero />
                     <div className="container mx-auto py-8 px-4">
-                        {projects.map((project, index) => (
+                        {projects && projects.map((project, index) => (
                             <ProjectCard key={index} project={project} />
                         ))}
                     </div>
