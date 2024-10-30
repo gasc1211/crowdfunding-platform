08d27660298e6b401618b67395309d4619a0b2d3
andre <andre.herrera@unah.hn>
2024-10-30 16:38:51 -0600

    arreglo menor

--- app/ui/components/ProjectCard.tsx
+++ app/ui/components/ProjectCard.tsx
@@ -1,4 +1,4 @@
-import { Button } from "@/components/ui/button";
+//import { Button } from "@/components/ui/button";
 import { Card } from "@/components/ui/card";
 import { Progress } from "@/components/ui/progress";
 import Details from "./Details";
