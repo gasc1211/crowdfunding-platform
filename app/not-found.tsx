8a55fee2aa39ff1ccfe9a286d16088aaa61d8eba
GASC1211 <48170476+Gasc1211@users.noreply.github.com>
2024-11-07 14:30:05 -0600

    Add 404 error page

---
+++ app/not-found.tsx
@@ -0,0 +1,14 @@
+import Link from "next/link";
+// import { apfel_fett } from "./ui/fonts";
+import Navbar from "./ui/components/Navbar";
+
+export default function NotFound() {
+  return (
+    <div className="w-dvw h-dvh flex flex-col items-center justify-center">
+      <Navbar />
+      <span className="text-8xl">🔍</span>
+      <h2 className="text-2xl font-bold pt-4"> Lo siento, parece que no encontramos la página que buscas...</h2>
+      <button className="mt-4 p-2 text-xl font-bold border rounded-md hover:bg-primary hover:text-primary-foreground"><Link href="/">Regresar a la Página Principal</Link></button>
+    </div>
+  );
+}
