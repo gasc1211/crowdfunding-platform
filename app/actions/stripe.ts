db810b362624dc6d6b9d0237226366bfc22a1b42
GASC1211 <48170476+Gasc1211@users.noreply.github.com>
2024-11-20 23:12:08 -0600

    Add payment intent method and move to actions folder

---
+++ app/actions/stripe.ts
@@ -0,0 +1,54 @@
+"use server";
+import { formatAmountForStripe } from "@/utils/stripe-helpers";
+import Stripe from "stripe";
+import * as config from "@/config";
+
+const domain = process.env.DOMAIN || "http://localhost:3000";
+const stripeSecretKey = process.env.STRIPE_SECRET_KEY as string;
+
+const stripe = new Stripe(stripeSecretKey); 
+
+// Crear sesión en la API de stripe para aceptar el pago
+export async function createCheckoutSession(values: {amount: number, embedded: boolean}, projectData: Project) {
+
+  const { amount, embedded } = values;
+  const { name, description, project_banner_url } = projectData;
+
+  const params: Stripe.Checkout.SessionCreateParams = {
+    line_items: [{
+      quantity: 1,
+      price_data: {
+        currency: config.CURRENCY,
+        product_data: {
+          name: name,
+          description: description || "",
+          images: [project_banner_url ? project_banner_url: ""]
+        },
+        unit_amount: formatAmountForStripe(amount, config.CURRENCY)
+      },
+    }],
+    mode: "payment",
+    ui_mode: embedded ? "embedded": "hosted",
+    return_url: `${domain}/return?session_id={CHECKOUT_SESSION_ID}`
+  };
+
+  const session: Stripe.Response<Stripe.Checkout.Session> = await stripe.checkout.sessions.create(params);
+  
+  return session;
+}
+
+export async function createPaymentIntent(
+  values: { amount: number },
+): Promise<{ client_secret: string }> {
+  const paymentIntent: Stripe.PaymentIntent =
+    await stripe.paymentIntents.create({
+      amount: formatAmountForStripe(
+        values.amount,
+        config.CURRENCY,
+      ),
+      automatic_payment_methods: { enabled: true },
+      currency: config.CURRENCY,
+    });
+
+  return { client_secret: paymentIntent.client_secret as string };
+}
