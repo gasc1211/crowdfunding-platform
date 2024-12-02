d14993cf0ca6a3e8f4146eb133dd1ae2010d40c1
GASC1211 <48170476+Gasc1211@users.noreply.github.com>
2024-12-02 14:08:16 -0600

    Add investment history button

--- app/ui/components/Sidebar.tsx
+++ app/ui/components/Sidebar.tsx
@@ -83,9 +83,12 @@ export default function Sidebar() {
                                 </Link>
                             </Button>
                         )}
+                        <Button className="w-full">
+                            <Link href={"/pagos/historial"}>Historial de Inversiones</Link>
+                        </Button>
                     </div>
                 </div>
             </CardContent>
-        </Card>
+        </Card >
     );
 }
